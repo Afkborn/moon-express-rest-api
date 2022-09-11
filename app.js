5fa2cb4b9b984d2a01219df2675fecc63ecb5808
Afkborn <kalaybilgehan60@gmail.com>
2022-09-11 14:04:55 +0300

    .

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const app = express();
 const bodyParser = require("body-parser");
 const dbConnect = require("./db/dbConnect");
 
+// Curb Cores Error by adding a header here
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
 app.use(bodyParser.json());
 
 // Import Routes
@@ -22,19 +36,7 @@ app.use("/test", testRoutes);
 
 dbConnect();
 
-// Curb Cores Error by adding a header here
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
+
 
 const port = process.env.PORT || 3000;
 
